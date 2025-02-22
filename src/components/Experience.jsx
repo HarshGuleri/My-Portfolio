5b569be42ff41d7b83d2bb5124d17ed1282fb42b
HarshGuleri <harsh379.be22@chitkara.edu.in>
2025-02-23 02:12:59 +0530

    further edits

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
   return (
     <>
       <div className="container ex" id="experience">
-        <h1>EXPERIENCE</h1>
+        <h1>CERTIFICATIONS</h1>
         {experience.map((data) => {
           return (
             <>
@@ -16,7 +16,7 @@ const Experience = () => {
                 data-aos-duration="1000"
               >
                 <div className="left">
-                  <img src={`/assets/${data.imageSrc}`} alt="" />
+                  <img src={`/assets/${data.imageSrc}`} alt="" style={{height:"50px"}}/>
                 </div>
                 <div className="right">
                   <h2>{data.role}</h2>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = () => {
       <div className="left nav_item"><h2>Harsh Guleri</h2></div>
       <div className="right">
         <a href="#home" className="nav_items">Home</a>
-        <a href="#experience" className="nav_items">Experience</a>
         <a href="#skills" className="nav_items">Skills</a>
         <a href="#projects" className="nav_items">Projects</a>
+        <a href="#experience" className="nav_items">Certifications</a>
         <a href="#contact" className="nav_items">Contact</a>
       </div>
     </div>
